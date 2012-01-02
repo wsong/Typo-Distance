46239550c330ef2b8e9eaa661ba3bdd180a2ce72
Wayne Song <wayne.e.song@gmail.com>
2012-01-01 20:08:38 -0800

    Added spaces

--- typodistance.py
+++ typodistance.py
@@ -10,6 +10,7 @@ qwertyKeyboardArray = [
     ['q','w','e','r','t','y','u','i','o','p','[',']','\\'],
     ['a','s','d','f','g','h','j','k','l',';','\''],
     ['z','x','c','v','b','n','m',',','.','/'],
+    ['', '', ' ', ' ', ' ', ' ', ' ', '', '']
     ]
 
 qwertyShiftedKeyboardArray = [
@@ -17,6 +18,7 @@ qwertyShiftedKeyboardArray = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"'],
     ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'],
+    ['', '', ' ', ' ', ' ', ' ', ' ', '', '']
     ]
 
 layoutDict = {'QWERTY': (qwertyKeyboardArray, qwertyShiftedKeyboardArray)}
